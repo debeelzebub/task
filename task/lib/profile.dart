265b7e7e3fd93c9af1d1f991b04fbad565111c90
debeelzebub <sruthysurendrants@gmail.com>
2022-07-08 19:47:25 +0530

    chNGE

--- task/lib/profile.dart
+++ task/lib/profile.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: dead_code, avoid_unnecessary_containers, unnecessary_this, avoid_print
+// ignore_for_file: dead_code, avoid_unnecessary_containers, unnecessary_this, avoid_print, unused_element
 
 import 'dart:ui';
 
@@ -16,86 +16,96 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-  SharedPreferences? sharepref;
+   SharedPreferences? sharepref;
   String name='';
+   List<String> list=[];
+
    final _tcontroller=TextEditingController();
-   
-     
+ 
    
       @override
   void initState(){
     super.initState();
-    savedata().whenComplete((){
-       setState(() {});
-     });
+  
+    
+    init();
   }
   @override
   void dispose(){
 
     super.dispose();
   }
-  bool _isSelected=false;
+    bool _isSelected=false;
   
   @override
   Widget build(BuildContext context) {
+      
+        list.add(name);
+  int listlen =list.length;
+  print(listlen);
+  print(list);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange[500],
         leading: const Icon(Icons.article_outlined),
-        title:const Text("INBOX HI"),
+        title:const Text("INBOX"),
         actions: const [
           Padding(padding: EdgeInsets.symmetric(horizontal: 20),
          child: Icon(Icons.search),),
-        ],
+       ],
         ),
       body: SafeArea(
         child:Column(
-          children: [
+        children: [
             const SizedBox(height: 50),
             Container(
               child: const CircleAvatar(radius:50,
               child:Image(image: AssetImage("assets/cc.jpg"))
               ),
-              ),
+            ),
             const SizedBox(
               height: 10
-              ),
+            ),
             Container( 
-                    child: Text('Hey, Dude',
-                     style: TextStyle(color: Colors.red[400], 
-                     fontSize: 25),
-                     ),
-                     ), 
-                       Divider(thickness: 5,),  
-                   Container(
-                    child:CheckboxListTile(
-                              title: Text('${sharepref?.getString("save_val")}'),
-                              value: _isSelected,
-                              onChanged: (bool? Value) { 
-                                          setState(() {
-                                            _isSelected = Value!; 
-                                          }); 
-                                        },
-                              controlAffinity: ListTileControlAffinity.leading,  //  <-- leading Checkbox
-                      )
-                   
-                   ) 
-          ],
+              child: Text('Hey, Dude',
+              style: TextStyle(color: Colors.red[400], 
+             fontSize: 25),
+             ),
+            ), 
+            const Divider(thickness: 5,) ,
+            Container(
+              child: Column(
+                children: [for(int i=1;i<listlen;i++)
+               CheckboxListTile(
+                title: Text(list[i]),
+                  value: _isSelected, onChanged:(bool? value){ 
+                    setState(() {
+                      _isSelected = value!;
+                    });
+
+                   },
+                   controlAffinity: ListTileControlAffinity.leading,
+                  ),
+              ],),
+            )
+       
+        ],
           
         ),
-                ),
-                 floatingActionButton: FloatingActionButton(
-                  onPressed: () async {
-               await openDialog();
-               setState(() {
-              }
-               );
-                 },
-         backgroundColor: Colors.orange[500],
-         child: const Icon(Icons.add)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+        await openDialog();
+        setState(() {
+        }
+        
+        );
+     
+        },
+        backgroundColor: Colors.orange[500],
+        child: const Icon(Icons.add)),
 
-      );
-      
+    );    
   }
   Future<String?> openDialog()=> showDialog<String>(
   context: this.context, 
@@ -115,20 +125,30 @@ class _ProfileState extends State<Profile> {
   actions: [
      ElevatedButton(onPressed:(){
       savedata();
+      init();
+        Navigator.pop(context);
       }, child: const Text('Submit'),  ),
-      // ElevatedButton(onPressed:(close),
-      //  child: const Text('close'),  )
   ],
   )
   );
-//   void close(){
-//    Navigator.of(this.context).pop(_tcontroller.text);
-//  } 
 
-savedata() async{
-print(_tcontroller.text);
-this.sharepref = await SharedPreferences.getInstance();
-this.sharepref?.setString('save_val', _tcontroller.text); 
- Navigator.of(this.context).pop(_tcontroller.text);
+  savedata() async{
+  print(_tcontroller.text);
+  // ignore: unused_local_variable
+  final sharepref = await SharedPreferences.getInstance();
+  this.sharepref?.setString('save_val', _tcontroller.text.toString());
+  }
+  
+ Future init() async {
+  sharepref = await SharedPreferences.getInstance();
+  final name=sharepref?.getString('save_val') as String;
+  setState(() {
+   this.name=name;
+  });
+  
+ }
+
+ 
+
 }
-}
+
